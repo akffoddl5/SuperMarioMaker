b41143b8ad512ac4a37b2ab4e985759337a0efa3
kjhjeonghyeon <80999210+kjhjeonghyeon@users.noreply.github.com>
2023-08-21 17:38:38 +0900

    거북이

--- Assets/kjh/Goomba.cs
+++ Assets/kjh/Goomba.cs
@@ -1,81 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class Goomba : MonoBehaviour
+public class Goomba : Enemy
 {
-
-
-    [Header("Collision Info")]
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private float groundCheckDistance;
-    [SerializeField] private Transform skyCheck;
-    [SerializeField] private float SkyCheckDistance;
-
-    [SerializeField] private LayerMask whatIsGround;
-    [SerializeField] private LayerMask whatIsPlayer;
-
-    public Rigidbody2D rb;
-    Animator anim;
-    float moveflip = 1;
-
-    bool move = true;
     // Start is called before the first frame update
-    void Start()
+   protected override void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        anim = GetComponentInChildren<Animator>();
+        base.Start();
     }
 
     // Update is called once per frame
-    void Update()
+   protected override void  Update()
     {
-        Move();
-        Flat();
+        base.Update();
+        TouchDown();
 
     }
 
-    private void Flat()
+    private void TouchDown()
     {
-        
-        if (IsSkyDetected())
+
+       
+        if (!IsSkyDetected())
         {
-            anim.SetBool("Flat", true);
-            move = false;
+            Move(true);
+        }
+        else
+        {
+            Move(false);
             Destroy(gameObject, 1);
         }
-    }
-
-    private void Move()
-    {
-        if (move)
-        {
-
-            rb.velocity = new Vector2(1 * moveflip, 0);
-
-            if (!IsGroundDetected())
-            {
-
-                transform.Rotate(0, 180, 0);
-
-
-                moveflip = moveflip * -1;
-
-            }
-        }
-    }
-
-    public bool IsGroundDetected() => Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
-    public bool IsSkyDetected() => Physics2D.Raycast(skyCheck.position, Vector2.up, groundCheckDistance, whatIsPlayer);
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x,
-            groundCheck.position.y - groundCheckDistance));
-
-        Gizmos.DrawLine(skyCheck.position, new Vector3(skyCheck.position.x,
-            skyCheck.position.y + SkyCheckDistance));
-
     }
 }
