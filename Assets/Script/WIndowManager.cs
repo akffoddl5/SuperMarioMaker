9e4984e374e3933ba6d8b92694b0f6bb9ba0258d
akffoddl5 <akffoddl5@naver.com>
2023-08-29 00:22:58 +0900

    window_manager

--- Assets/Script/WIndowManager.cs
+++ Assets/Script/WIndowManager.cs
@@ -10,18 +10,41 @@ public class WIndowManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        RenderTexture renderTexture = GetComponent<Camera>().targetTexture;
-        Texture2D texture = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.ARGB32, false);
-        RenderTexture.active = renderTexture;
-        texture.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
-        texture.Apply();
+        //RenderTexture renderTexture = GetComponent<Camera>().targetTexture;
+        //Texture2D texture = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.ARGB32, false);
+        //RenderTexture.active = renderTexture;
+        //texture.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+        //texture.Apply();
 
-        File.WriteAllBytes($"{Application.dataPath}/{screenShotName}.png", texture.EncodeToPNG());
+        //File.WriteAllBytes($"{Application.dataPath}/{screenShotName}.png", texture.EncodeToPNG());
+    }
+
+    
+
+    IEnumerator TakeScreenShot()
+    {
+        Debug.Log("shot");
+        yield return new WaitForEndOfFrame();
+
+
+        var width = Screen.width;
+        var height = Screen.height;
+        var tex = new Texture2D(width, height, TextureFormat.RGB24, false);
+
+
+        tex.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+        tex.Apply();
+        File.WriteAllBytes($"{Application.dataPath}/{screenShotName}.png", tex.EncodeToPNG());
+
+        Debug.Log("shot end");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.F12))
+        {
+            StartCoroutine(TakeScreenShot());
+        }
     }
 }
