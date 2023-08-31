0355e28fa7ca6e4ab35fdaf1382f97266c025b5e
jiwon824 <p01046108755@gmail.com>
2023-08-31 19:06:00 +0900

    독무대 하는 중....(여기서부터 시작함)

--- Assets/Script/FinishSoloStage.cs
+++ Assets/Script/FinishSoloStage.cs
@@ -7,7 +7,7 @@ public class FinishSoloStage : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.GetComponent<RectTransform>().localScale = new Vector2(46837, 26346);
     }
 
     // Update is called once per frame
