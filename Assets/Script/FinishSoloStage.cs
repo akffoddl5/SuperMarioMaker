b36c28c6d7fd3f98dbecf9693365841330f66c57
jiwon824 <p01046108755@gmail.com>
2023-08-31 20:01:13 +0900

    독무대 왜 안 되지....

--- Assets/Script/FinishSoloStage.cs
+++ Assets/Script/FinishSoloStage.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class FinishSoloStage : MonoBehaviour
 {
+    public bool isFinishGame = false;
+    bool isCoroutineStart = false;
+    Vector2 minusVector = new Vector2(0.1f, 0.1f); 
+    Vector2 finishScale = new Vector2(3, 3);
     // Start is called before the first frame update
     void Start()
     {
-        transform.GetComponent<RectTransform>().localScale = new Vector2(46837, 26346);
+        isFinishGame = true;
+        transform.localPosition = Vector3.zero;
+        transform.localScale = new Vector2(40, 40);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isFinishGame && !isCoroutineStart)
+        {
+            isCoroutineStart = true;
+            StartCoroutine(FinishGame_EF());
+        }
+    }
+    IEnumerator FinishGame_EF()
+    {
+        while(transform.localScale.x <= finishScale.x)
+        {
+            Debug.Log("IEnumerator FinishGame_EF() µé¾î¿È!!!");
+            Vector2 tmp = new Vector2(transform.localScale.x - minusVector.x, transform.localScale.y - minusVector.y);
+            transform.localScale = tmp;
+            yield return new WaitForSeconds(0.5f);
+
+        }
     }
 }
