21c8cec65082c44834e276b4c3e4009f6ff56ae1
Kim-Man-Jun <118789321+Kim-Man-Jun@users.noreply.github.com>
2023-09-06 12:29:22 +0900

    09.06/KMJ/Pipe

--- Assets/Kdm/Script/Pipe_top.cs
+++ Assets/Kdm/Script/Pipe_top.cs
@@ -97,7 +97,7 @@ public class Pipe_top : MonoBehaviour
         //}
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
